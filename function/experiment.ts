ba59abb8e490e8305b25b45f42b4d0e9417df525
bryan athallah <bryanathallah7@gmail.com>
2023-07-29 18:07:58 +0700

    almost done

--- function/experiment.ts
+++ function/experiment.ts
@@ -1,12 +1,21 @@
+import {Person} from "./person";
+
 interface HeroType {
     (name:string,age:number,power:string): object
 }
 
-type SuperHero = {
+interface Man {
     name: string,
     age: number,
     power: string
 }
+
+interface Costum {
+    mask: string,
+    cap: string,
+    armor: string
+}
+
 const createHero: HeroType = (name: string, age: number, power:string): object => {
     return {
         name: name,
@@ -15,12 +24,29 @@ const createHero: HeroType = (name: string, age: number, power:string): object =
     }
 }
 
+const Batman: Man & Costum = {
+    name: "Bruce Wayne",
+    age: 32,
+    power: "money",
+    mask: "black mask",
+    cap: "black cap",
+    armor: "black anti bullet"
+}
+
 const bryan: object = createHero("bryan", 19, "money")
-console.log(bryan, " by object")
-console.log(createHero("bryan", 19, "money"), " by function")
+// console.log(bryan, " by object")
+// console.log(createHero("bryan", 19, "money"), " by function")
 
 function menghindar(a: number, b: number) : void {
     const hasil = a + b;
     console.log(hasil)
 }
-menghindar(2,3)
+// menghindar(2,3)
+
+const person: any = {
+    name: "bryan",
+    age: 19
+}
+
+const person2: Person = person as Person
+console.info(person.sayHello("budi"))
--- function/funcInterface.ts
+++ function/funcInterface.ts
@@ -4,3 +4,13 @@ export interface add {
 const addNumber: add = (value1:number, value2: number): number => {
     return value1 + value2
 }
+
+function sayHello(name?:string): string {
+    if (name) {
+        return `halooo ${name}`
+    } else {
+        return "haloo saja"
+    }
+}
+
+console.log(sayHello())
